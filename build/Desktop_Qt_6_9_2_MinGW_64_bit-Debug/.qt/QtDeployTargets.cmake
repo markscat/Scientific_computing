2657e7a1ddb738a027b2f96f90085b7e006fae4c
markscat <markscat@gmail.com>
2026-01-02 17:14:36 +0800

    Delete build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Scientific_computing_FILE D:/for work/temp/workshorp/PC/Qt/Scientific_computing/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/Scientific_computing.exe)
-set(__QT_DEPLOY_TARGET_Scientific_computing_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Scientific_computing_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
